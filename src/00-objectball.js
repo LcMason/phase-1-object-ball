e88c362d6058f4519f835bb4aeae6fcc15a87410
Lionel Mason <capitalcookhouse@gmail.com>
2021-11-23 20:42:56 -0500

    Done.

---
+++ src/00-objectball.js
@@ -0,0 +1,13 @@
+const { assertTSExpressionWithTypeArguments } = require("babel-types");
+const { black } = require("color-name");
+
+const gameObject = {
+    home: value1,
+    away: value2, {
+        teamName: 'Brooklyn Nets'
+        colors: 'Black', 'White',
+        players:
+    
+
+
+
